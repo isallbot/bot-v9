766ab2c440cddada354d5055ae36f5d98b0e9f48
isallbot <78047216+isallbot@users.noreply.github.com>
2021-01-30 15:13:20 +0700

    Add files via upload

---
+++ plugins/info.js
@@ -0,0 +1,38 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  conn.reply(m.chat, `
+══════════════════════════
+║╠═════〘 INFO BOT 〙════════
+║╠➥ Dibuat dengan bahasa javascript via NodeJs
+║╠➥ Rec: FAISAL WAPI
+║╠➥ Script: B0T IS4LL
+║╠➥ Instagram: @faisall_wapi
+║╠➥ YouTube: ISALL GANS 
+║╠➥ Tiktok: @gans.nih
+║╠═════〘 DONASI 〙══════════
+║╠═〘 DONASI 〙 ═
+║╠➥ *GOPAY* : 0896-3026-7618
+║╠➥ *TRI* : 0896-3026-7618
+║╠➥ *SAWERIA* : https://saweria.co/BOTISALL
+║║
+║╠➥Request? Wa.me/6289630267618
+║║
+║╠════〘 BOT ISALL V3 〙═══════
+═══════════════════════════
+`.trim(), m)
+}
+handler.help = ['info']
+handler.tags = ['about']
+handler.command = /^(info)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
